5b418abd98c40ecb7a2920d803dccad6c490d95f
jinendrabanthia <jinendra.banthia.iter@gmail.com>
2026-07-16 02:56:27 +0530

    refactor: Update DomeGallery implementation with gesture controls

--- src/components/shared/DomeGallery.tsx
+++ src/components/shared/DomeGallery.tsx
@@ -1,5 +1,6 @@
-"use client";
 // @ts-nocheck
+"use client";
+
 import { useEffect, useMemo, useRef, useCallback } from 'react';
 import { useGesture } from '@use-gesture/react';
 import './DomeGallery.css';
